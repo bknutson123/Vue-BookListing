4cde465726c6c6edb46b7e9435d269daa5415287
Ben Knutson <bentknutson@gmail.com>
2019-05-29 23:12:38 -0600

    loop through different books.

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    </ul>
   </div>
 </template>
 
@@ -10,7 +13,12 @@
     name: 'BookList',
     data() {
       return {
-        title: 'All Books'
+        title: 'All Books',
+        books: [
+          {title: 'book1', author: 'author1'},
+          {title: 'book2', author: 'author2'},
+          {title: 'book3', author: 'author3'}
+        ]
       };
     }
   };
@@ -19,5 +27,13 @@
 <style>
   h1, h2 {
     font-weight: normal
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: block;
+    margin: 0 10px;
   }
 </style>
